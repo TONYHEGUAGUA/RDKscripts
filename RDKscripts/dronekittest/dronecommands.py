5c83e660d7d682b278f1915f82cdb153cfc05d1f
TONYHEGUAGUA <tonyhe_2015321@outlook.com>
2025-06-03 21:35:26 +0800

    把测试文件移到了dronekittest里面

---
+++ RDKscripts/dronekittest/dronecommands.py
@@ -0,0 +1,153 @@
+from collections import abc
+import collections
+collections.MutableMapping = abc.MutableMapping
+from typing import Tuple
+from pymavlink import mavutil
+from dronekit import Vehicle
+from dronekit import VehicleMode
+import math
+import time
+
+def condition_yaw(vehicle: Vehicle,heading, relative=False):
+    """
+    Send MAV_CMD_CONDITION_YAW message to point vehicle at a specified heading (in degrees).
+
+    This method sets an absolute heading by default, but you can set the `relative` parameter
+    to `True` to set yaw relative to the current yaw heading.
+
+    By default the yaw of the vehicle will follow the direction of travel. After setting 
+    the yaw using this function there is no way to return to the default yaw "follow direction 
+    of travel" behaviour (https://github.com/diydrones/ardupilot/issues/2427)
+
+    For more information see: 
+    http://copter.ardupilot.com/wiki/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_condition_yaw
+    """
+    if relative:
+        is_relative = 1 #yaw relative to direction of travel
+    else:
+        is_relative = 0 #yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+        0, #confirmation
+        heading,    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        1,          # param 3, direction -1 ccw, 1 cw
+        is_relative, # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+
+
+def arm_and_takeoff(vehicle: Vehicle,aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode    = VehicleMode("GUIDED")
+    vehicle.armed   = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        if not(vehicle.mode == VehicleMode("GUIDED")):
+            break;
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        #Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+def setYaw(vehicle: Vehicle, relativeYaw: float) -> None:
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW,
+        0, #confirmation
+        abs(relativeYaw),    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        1 if relativeYaw >= 0 else -1, # param 3, direction -1 ccw, 1 cw
+        1, # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+
+    vehicle.send_mavlink(msg)
+    vehicle.flush()
+
+def send_body_velocity(vehicle: Vehicle,velocity_x, velocity_y, velocity_z):
+    #发送基于机体坐标系的速度移动指令
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_BODY_NED, # frame
+        0b0000111111000111, # type_mask (only speeds enabled)
+        0, 0, 0, # x, y, z positions (not used)
+        velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
+        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink) 
+    vehicle.send_mavlink(msg)
+
+
+
+def send_ned_velocity(vehicle: Vehicle,velocity_x, velocity_y, velocity_z):
+    """
+    Move vehicle in direction based on specified velocity vectors and
+    for the specified duration.
+
+    This uses the SET_POSITION_TARGET_LOCAL_NED command with a type mask enabling only 
+    velocity components 
+    (http://dev.ardupilot.com/wiki/copter-commands-in-guided-mode/#set_position_target_local_ned).
+    
+    Note that from AC3.3 the message should be re-sent every second (after about 3 seconds
+    with no message the velocity will drop back to zero). In AC3.2.1 and earlier the specified
+    velocity persists until it is canceled. The code below should work on either version 
+    (sending the message multiple times does not cause problems).
+    
+    See the above link for information on the type_mask (0=enable, 1=ignore). 
+    At time of writing, acceleration and yaw bits are ignored.
+    """
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
+        0b0000111111000111, # type_mask (only speeds enabled)
+        0, 0, 0, # x, y, z positions (not used)
+        velocity_x, velocity_y, velocity_z, # x, y, z velocity in m/s
+        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink) 
+    vehicle.send_mavlink(msg)
+    
+def setPositionTarget(vehicle: Vehicle, position: Tuple[float, float], yawRate: float) -> None:
+    localNorth, localEast = position
+    # Find altitude target for NED frame
+    ignoreVelocityMask =  0b111000
+    ignoreAccelMask =  0b111000000
+    ignoreYaw = 0b10000000000
+    emptyMask = 0b0000000000000000
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED, # Use offset from current position
+        emptyMask + ignoreAccelMask + ignoreVelocityMask + ignoreYaw, # type_mask
+        localNorth, localEast, 0,
+        0, 0, 0, # x, y, z velocity in m/s (not used)
+        0, 0, 0, # x, y, z acceleration (not used)
+        0, math.radians(yawRate))    # yaw, yaw_rate
+
+    vehicle.send_mavlink(msg)
